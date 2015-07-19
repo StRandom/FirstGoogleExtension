324ab348cec93cd013a2e17c4ff3d5a28b1cea30
Dmitry Malikhtarovich <dimrandom@gmail.com>
2015-07-20 00:17:18 +0600

    Storage definition
    +storage permission;
    *fix a little bit.

--- popup.js
+++ popup.js
@@ -1,15 +1,17 @@
 document.getElementById("gysearch").onsearch = onSearch;
 document.addEventListener("search", onSearch);
-addEventListener("onsearch",onSearch);
+
 function onSearch() {
    			var x = document.getElementById("gysearch");
 			var y = document.getElementById("SSDropDown");
 			if (y.value == "Google") {
+				document.body.style.backgroundColor = "blue";
 				chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
       chrome.tabs.update(tab.id, {url: "https://www.google.ru/webhp?sourceid=chrome-instant&rlz=1C1CHMO_enRU594RU594&ion=1&espv=2&ie=UTF-8#q=" + x.value});
 });
      
 			} else {
+				document.body.style.backgroundColor = "red";
 				chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
       chrome.tabs.update(tab.id, {url: "https://www.yandex.com/search/?lr=87&text=" + x.value});
 });
